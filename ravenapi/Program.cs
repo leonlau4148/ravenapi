c77def2cf8216eafdf46e0080e6eb8114bda841d
ADRALES, RAVEN PAUL S. <148683955+leonlau4148@users.noreply.github.com>
2026-06-11 16:56:49 +0800

    delete weather

--- ravenapi/Program.cs
+++ ravenapi/Program.cs
@@ -90,12 +90,8 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors("AllowFlutterApp");
 app.UseAuthentication();
